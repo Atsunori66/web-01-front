0ef28571d3b22540e54fd902de4fd75c6c924b03
Atsunori66 <atsunori.smith@gmail.com>
2024-11-10 22:36:53 +0900

    test 2024/11/10 api_location set to blank

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,13 +4,6 @@ import axios from "axios";
 import FormData from "form-data";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { DefaultAzureCredential } from "@azure/identity";
-// const { DefaultAzureCredential } = require('@azure/identity');
-// const { BlobServiceClient } = require("@azure/storage-blob");
-// import {config} from "dotenv"
-// require("dotenv").config();
-
-// const url = "https://prod-23.japaneast.logic.azure.com:443/workflows/5a7ce2df20b748b48a3f02f80b16ff62/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=FrRi6LcsjoYqqMxJPLgWyqdUTdcIWi-xhAwWJ94bYmI";
-// const url = "https://logic-02-api-standard.azurewebsites.net:443/api/lyrixer-api/triggers/When_a_HTTP_request_is_received/invoke?api-version=2022-05-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=vZAoAylqEelI73Ma1JqEbfo7UAxvIba8ldFGS1tXpOM";
 
 const url = process.env.LOGIC_APP_STANDARD_URL;
 if (!url) throw Error("Logic App url not found");
@@ -22,13 +15,6 @@ const blobServiceClient = new BlobServiceClient(
   `https://${accountName}.blob.core.windows.net`,
   new DefaultAzureCredential()
 );
-
-// デフォルトの bodyParser を無効化
-// export const config = {
-//   api : {
-//     bodyParser : false
-//   }
-// }
 
 export async function POST(req: Request) {
   const incomingFormData = await req.formData();
