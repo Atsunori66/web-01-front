75a6b7db8a06286766c4ac8222c48ee4ab78bb32
Atsunori66 <atsunori.smith@gmail.com>
2024-12-10 23:15:00 +0900

    2024/12/10 added opengraph

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,27 +1,25 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { i18n, type Locale } from "../i18n/i18n-config";
 import { Providers } from "../providers";
 import CookieBanner from "../components/cookie-banner";
 import GoogleAdsense from "../components/google-adsense";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900"
-});
-
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900"
-});
-
 export const metadata: Metadata = {
   title: "LyriXer - Lyrics Extractor AI",
-  description: "This is your new favorite AI lyrics extractor! Quickly and accurately extract lyrics from any song, 100% free."
+  description: "This is your new favorite AI lyrics extractor! Quickly and accurately extract lyrics from any song, 100% free.",
+  openGraph: {
+    title: "LyriXer - Lyrics Extractor AI",
+    description: "Extracts lyrics from any song. 100% free."
+  },
+  twitter: {
+    title: "LyriXer - Lyrics Extractor AI",
+    description: "Extracts lyrics from any song. 100% free.",
+    card: "summary_large_image"
+  }
 };
 
 export async function generateStaticParams() {
@@ -39,7 +37,7 @@ export default async function RootLayout(
 
   return (
     <html lang={ params.lang } suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
         <Providers>
           { children }
           <CookieBanner/>
